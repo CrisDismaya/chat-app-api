a1a9ae22237d7ad2164593225fe2d9de8c996dc5
CrisDismaya <cris.dismaya20@gmail.com>
2024-02-24 17:35:10 +0800

    fix(Contact Model): Added fillable and hidden and changes in user modal for hidden column

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
+use Laravel\Sanctum\HasApiTokens;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'message',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
         'email_verified_at',
         'created_at',
         'updated_at',
+        'deleted_at'
     ];
 
     /**
